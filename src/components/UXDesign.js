9124063cb8bab92fcc0bc46a7a246342fee415b7
Jessica Peng <jesspeng@Jessicas-MacBook-Pro-3.local>
2023-06-02 12:10:04 -0400

    changed index location

--- src/components/UXDesign.js
+++ src/components/UXDesign.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Contact from './Contact';
 
 import Footer from './Footer';
+import {Flex} from "@adobe/react-spectrum";
 
 class UXDesign extends Component {
     render() {
@@ -11,48 +12,77 @@ class UXDesign extends Component {
                     <h4 key="AEP-Copy-App">Adobe Experience Platform<br/>Artifact Copy App</h4><br></br>
                     <img id="full-width" key="overview" src="/project_images/AEPArtifactCopyApp/overview.png" alt="overview"/><br/><br/>
                     <div className="row">
-                        <h4>The Story</h4><br/>
+                        <h5>Background</h5><br/>
                         <p>Adobe Experience Platform allows customers to store, manage, and monitor configurations of
                             data or <b>artifacts</b> in sandboxes. Transferring complex artifacts can be
-                            time-consuming and error prone for multi-sandbox environments. My team's primary objective was to
+                            time-consuming and error prone for multi-sandbox environments. <u>My team's primary objective was to
                             design and build an app that enables users to easily copy Adobe Experience Platform (AEP)
-                            and Adobe Journey Optimizer (AJO) artifacts between sandboxes for seamless deployments.<br/><br/>
+                                and Adobe Journey Optimizer (AJO) artifacts between sandboxes for seamless deployments.</u><br/><br/>
                             <b>My role was Lead Front End Developer in a team of five.</b><br/><br/>
-                            The app was developed using React Spectrum, App Builder (UI template), and SPA Pipeline
+                            Technologies: React Spectrum, App Builder (UI template), and SPA Pipeline
                             for deployment to Unified Shell.<br/><br/>
                         </p><br/>
-                        <h4>Design Process</h4><br/>
-                        <p>We worked with solution architects and consultants to outline the list of requirements for the app below:</p><br/>
-                        <ul>
-                            <li>Copy all foundation artifacts (schemas, datasets, segments, sources, destinations) and
-                                first level dependencies </li>
-                            <li>Sync - compare artifacts between the source and destination sandbox, allow user to sync
-                                the destination artifact </li>
-                            <li>Rollback - allow user to revert any changes made after sync </li>
-                            <li>Audit Logs - log all actions in the app for documentation and troubleshooting</li>
-                        </ul>
+                        <h5>Requirements</h5><br/>
+                        <img id="full-width" key="milestones" src="/project_images/AEPArtifactCopyApp/milestones.png" alt="milestones"/><br/><br/><br/>
+                        <p>The app needed to accomplish the following:</p><br/>
+                        <Flex direction="row">
+                            <Flex direction="column" width="50%">
+                                <p id="list">
+                                    • Copy all foundation artifacts (schemas, datasets, segments, sources, destinations) and
+                                    first level dependencies<br/>
+                                    • Review dependencies before initiating copy<br/>
+                                </p>
+                            </Flex>
+                            <Flex direction="column" width="50%">
+                                <p id="list">
+                                    • Sync artifacts between the source and destination sandboxes<br/>
+                                    • Revert artifacts (once) after syncing<br/>
+                                    • Log actions for documentation and troubleshooting
+                                </p>
+                            </Flex>
+                        </Flex>
                         <h5>Initial Design</h5><br/>
+                        <Flex direction="row">
+                            <Flex direction="column">
+                                <img id="half-width" key="sketch1" src="/project_images/AEPArtifactCopyApp/sketch1.jpg" alt="sketch"/><br/><br/><br/>
+                            </Flex>
+                            {/*<Flex direction="column">*/}
+                            {/*    <img id="half-width" key="sketch2" src="/project_images/AEPArtifactCopyApp/sketch2.jpg" alt="sketch"/><br/><br/><br/>*/}
+                            {/*</Flex>*/}
+                            <Flex direction="column">
+                                <img id="half-width" key="sketch3" src="/project_images/AEPArtifactCopyApp/sketch3.jpg" alt="sketch"/><br/><br/><br/>
+                            </Flex>
+                        </Flex>
                         <p>The first design consisted of two pages: the copy page and confirmation page.<br/>
                             On the copy page, the user can select the source and destination sandbox and artifacts to
-                            display from the left panel. All artifacts are listed on the right. When the user selects an
+                            display from the left panel. All artifacts are listed on the right.</p><br/>
+                        <img id="full-width" key="initial" src="/project_images/AEPArtifactCopyApp/initial-design.png" alt="initial"/><br/><br/><br/>
+                        <img id="full-width" key="dep" src="/project_images/AEPArtifactCopyApp/dependency.png" alt="dep"/><br/><br/><br/>
+                        <p>When the user selects an
                             artifact, the dependency graph updates below. Once the user confirms all artifacts to be
                             copied, an audit log shows the progress of backend calls in real time.<br/>
                             The user is then directed to the confirmation page, which lists all successes and failures
-                            and artifacts in the destination sandbox.<br/>
-                            Setbacks<br/>
-                            After we demoed the app to internal consultants and solution architects, we narrowed down
-                            our main issues to troubleshooting and scalability. Users who want to copy multiple
+                            and artifacts in the destination sandbox.</p><br/>
+                        <h5>Setbacks</h5><br/>
+                        <p>Our main issues were troubleshooting and scalability. Users who want to copy multiple
                             artifacts cannot view all dependencies at once. Backend calls for artifact types often
                             overlapped, so troubleshooting from the audit log was difficult. Additional features like
-                            syncing and reverting would also be tough to integrate in this UI.<br/>
-                            <b>The primary focus of the redesign was to separate artifacts into their own copy workflows.</b>
-                        </p>
-                        <h5>Final Design</h5><br/>
-                        <p>In the redesign, I followed existing design layouts found throughout Experience Platform.
+                            syncing and reverting would also be tough to integrate in this UI.
+                        </p><br/>
+                        <h5>Redesign</h5><br/>
+                        <p>The primary focus of the redesign was to separate artifact types into their own copy workflows.<br/>
+                            <br/>I followed existing design patterns found throughout Experience Platform.</p><br/>
+                        <Flex direction="row">
+                            <Flex direction="column">
+                                <img id="half-width" key="aep" src="/project_images/AEPArtifactCopyApp/sketch1.jpg" alt="aep"/><br/><br/><br/>
+                            </Flex>
+                            <Flex direction="column">
+                                <img id="half-width" key="figma" src="/project_images/AEPArtifactCopyApp/sketch3.jpg" alt="figma"/><br/><br/><br/>
+                            </Flex>
+                        </Flex>
                             <a href="https://www.figma.com/proto/TIHIYPfyhvmQiSWxfzz5i1/Untitled?node-id=1-400&scaling=
-                            min-zoom&page-id=0%3A1&starting-point-node-id=1%3A137">Figma</a><br/>
-                            Improvements<br/>
-                            Instead of copying all artifact types at once, the user can now select an artifact from the
+                            min-zoom&page-id=0%3A1&starting-point-node-id=1%3A137">Link to redesign in Figma</a><br/><br/>
+                            <p>Instead of copying all artifact types at once, the user can now select an artifact from the
                             left panel. The following steps then apply to all artifacts to copy:
                             <ul>
                                 <li>Choose a source sandbox</li>
