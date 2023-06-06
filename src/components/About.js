729c61a3dbe478091d05fa36bd188c1edaecebd5
Jessica Peng <jesspeng@Jessicas-MacBook-Pro-3.local>
2023-06-06 15:45:52 -0400

    mobile styling fixes

--- src/components/About.js
+++ src/components/About.js
@@ -11,10 +11,9 @@ class About extends Component {
                 <div className="content-container about">
                     <h1 className="h1 about">about me</h1><br></br>
                     <div className="row">
-                        <h4>Hi! I'm Jess.</h4><p>I am currently a Tech Consultant at Adobe. 
-                        I majored in Computer Science and Computer Graphics in the <span><a href="http://cg.cis.upenn.edu/dmd.html" target="_blank" rel="noreferrer">Digital Media Design</a></span> program and minored 
-                        in Fine Arts at the University of Pennsylvania, where I graduated from the School of Engineering in May 2019. As a student, I was involved in the Society of Women Engineers, <i>The Daily Pennsylvanian</i>, Weiss Tech House, and Alpha Phi communities and interned at <i>The New York Times</i> and Molex.<br></br><br></br>
-                        In my free time, I like drawing pet portraits, cartooning on @peng.and.ink, catching up on episodes of <i>The Bachelor</i>, and running. <br></br><br></br></p>
+                        <h4>Hi! I'm Jess.</h4><p>I am a Software Engineer on the DX Customer Engineering team at Adobe based in NYC. I majored in Computer Science and Computer Graphics in the <span><a href="http://cg.cis.upenn.edu/dmd.html" target="_blank" rel="noreferrer">Digital Media Design</a></span> program and minored
+                        in Fine Arts at the University of Pennsylvania, where I graduated from the School of Engineering in May 2019.<br></br><br></br>
+                        In my free time, I love drawing pet portraits, posting cartoons on @officialceojess, running to a good playlist, and catching up on episodes of my favorite comedy/reality shows.<br></br><br></br></p>
                                 
                         {/* <img id='cartoon' src={cartoon} alt="cartoon"/> */}
                     </div>
--- src/components/UXDesign.js
+++ src/components/UXDesign.js
@@ -21,7 +21,7 @@ const UXDesign = () => {
                                 tabIndex="0"
                                 className="carousel__slide">
                                 <div className="carousel__snapper">
-                                    <img key="overview" src="https://i.postimg.cc/JzzWQbYN/overview.png"/>
+                                    <img key="source" src="https://i.postimg.cc/L6CBNnFY/selectsource.png"/>
                                     <a href="#carousel__slide4"
                                        className="carousel__prev">Go to last slide</a>
                                     <a href="#carousel__slide2"
@@ -32,6 +32,7 @@ const UXDesign = () => {
                                 tabIndex="0"
                                 className="carousel__slide">
                                 <div className="carousel__snapper"></div>
+                                <img key="select-sandbox" src="https://i.postimg.cc/Z56n0MHb/selectsandboxes.png"/>
                                 <a href="#carousel__slide1"
                                    className="carousel__prev">Go to previous slide</a>
                                 <a href="#carousel__slide3"
@@ -41,6 +42,7 @@ const UXDesign = () => {
                                 tabIndex="0"
                                 className="carousel__slide">
                                 <div className="carousel__snapper"></div>
+                                <img key="select-destination-sandbox" src="https://i.postimg.cc/yNPNy322/selectdestsandbox.png"/>
                                 <a href="#carousel__slide2"
                                    className="carousel__prev">Go to previous slide</a>
                                 <a href="#carousel__slide4"
@@ -50,6 +52,7 @@ const UXDesign = () => {
                                 tabIndex="0"
                                 className="carousel__slide">
                                 <div className="carousel__snapper"></div>
+                                <img key="copy" src="https://i.postimg.cc/fRy5NYff/sandboxcopy.png"/>
                                 <a href="#carousel__slide3"
                                    className="carousel__prev">Go to previous slide</a>
                                 <a href="#carousel__slide1"
@@ -87,13 +90,14 @@ const UXDesign = () => {
                                 (AEP)
                                 and Adobe Journey Optimizer (AJO) artifacts between sandboxes for seamless
                                 deployments.</u><br/><br/>
-                            <b>My role was Lead Front End Developer in a team of five.</b><br/><br/>
+                            My role was Lead Front End Developer in a team of five.<br/><br/>
                             Technologies: React Spectrum, App Builder (UI template), and SPA Pipeline
                             for deployment to Unified Shell.<br/><br/>
                         </p><br/>
                         <h5>Requirements</h5><br/>
                         <img id="full-width" key="milestones" src="https://i.postimg.cc/G217XR3W/milestones.png"
-                             alt="milestones"/><br/><br/><br/>
+                                 alt="milestones"/>
+                        <br/><br/><br/>
                         <p>The app needed to accomplish the following:</p><br/><br/>
                         <p id="list">
                             • Copy all foundation artifacts (schemas, datasets, segments, sources, destinations) and
@@ -112,62 +116,92 @@ const UXDesign = () => {
                             artifacts in the destination sandbox.<br/>
                         </p>
                         <div class="row">
-                            <div class="col-xs-6">
-                                <img id="half-width" key="sketch1" src="https://i.postimg.cc/RFRRGnDt/sketch1.jpg" alt="sketch"/><br/><br/>
+                            <div class="col-6">
+                                <img class="ux-design-img" key="sketch1" src="https://i.postimg.cc/RFRRGnDt/sketch1.jpg"
+                                     alt="sketch"/><br/><br/>
                             </div>
-                            <div className="col-xs-6">
-                                <img id="half-width" key="sketch3" src="https://i.postimg.cc/prcBMMxX/sketch3.jpg" alt="sketch"/><br/><br/>
+                            <div class="col-6">
+                                <img class="ux-design-img" key="sketch3" src="https://i.postimg.cc/prcBMMxX/sketch3.jpg"
+                                     alt="sketch"/><br/><br/>
                             </div>
                         </div>
-                        <img id="full-width" key="initial" src="https://i.postimg.cc/zGgF6QQ8/initial-design.png"
-                             alt="initial"/><br/>
-                        <img id="full-width" key="dep" src="https://i.postimg.cc/FKv3qHcV/dependency.png"
-                             alt="dep"/><br/>
-                        <p><br/>Dependency tree</p>
-                        <p><h5><br/>Setbacks</h5>After several internal demos and testing sessions, we concluded that our initial design lacked
+                        <div class="row">
+                            <img id="full-width" key="initial" src="https://i.postimg.cc/zGgF6QQ8/initial-design.png"
+                                 alt="initial"/><br/><br/>
+                        </div>
+                        <div class="row">
+                            <img id="full-width" key="dep" src="https://i.postimg.cc/FKv3qHcV/dependency.png"
+                                 alt="dep"/><br/><br/>
+                        </div>
+                        <p><br/>Dependency tree<br/></p>
+                        <div>
+                            <h5><br/>Setbacks</h5><p>After several internal demos and testing sessions, we concluded that our initial design lacked
                             scalability and helpful reporting. For large copy operations,
                             users could not review all dependencies at once. Troubleshooting from the audit log was also
                             difficult due to the volume of calls being made. Lastly, adding additional features like
                             sync and revert would crowd the UI.
                         </p><br/>
-                        <h5><br/>Redesign</h5><br/>
+                        </div>
+                        <div>
+                        <h5>Redesign</h5>
                             <p>Based on existing design patterns in AEP (left), I drafted a re-design in
                             <a href="https://www.figma.com/proto/TIHIYPfyhvmQiSWxfzz5i1/Untitled?node-id=1-400&scaling=
-                            min-zoom&page-id=0%3A1&starting-point-node-id=1%3A137"> Figma</a>.</p><br/>
-                        <div className="row">
-                            <div className="col-xs-6">
-                                <img id="half-width" key="aep" src="https://i.postimg.cc/kGVdm98T/Screen-Shot-2023-06-02-at-5-53-33-PM.png" alt="aep"/>
+                            min-zoom&page-id=0%3A1&starting-point-node-id=1%3A137"> Figma</a>.</p>
+                        <div class="row">
+                            <div class="col-6">
+                                <img class="ux-design-img" key="aep" src="https://i.postimg.cc/kGVdm98T/Screen-Shot-2023-06-02-at-5-53-33-PM.png" alt="aep"/>
                             </div>
-                            <div className="col-xs-6">
-                                <img id="half-width" key="figma" src="https://i.postimg.cc/brQfJgCn/Screen-Shot-2023-06-02-at-5-55-12-PM.png" alt="figma"/>
+                            <div class="col-6">
+                                <img class="ux-design-img" key="figma" src="https://i.postimg.cc/brQfJgCn/Screen-Shot-2023-06-02-at-5-55-12-PM.png" alt="figma"/>
                             </div>
                         </div><br/>
                         <p>Problem #1: Copying all artifact types at once leads to confusion and is difficult to troubleshoot.<br/>
                             Solution: Separate each artifact into its own tab.</p>
-                        <img id="full-width" key="improv1" src="https://i.postimg.cc/DzMKKj93/Screen-Shot-2023-06-02-at-11-43-34-PM.png" alt="improv1"/>
+                        <div className="row">
+                            <img id="full-width" key="improv1" src="https://i.postimg.cc/DzMKKj93/Screen-Shot-2023-06-02-at-11-43-34-PM.png" alt="improv1"/>
+                            <br/><br/>
+                        </div>
                         <p><br/>Problem #2: Users can only view one dependency tree at a time.<br/>
                             Solution: A dialog window clearly displays each artifact and its dependencies in list form.</p>
-                        <img id="full-width" key="improv2" src="https://i.postimg.cc/GmDMpMT1/Screen-Shot-2023-06-02-at-11-57-11-PM.png" alt="improv2"/>
+                        <div className="row">
+                            <img id="full-width" key="improv2" src="https://i.postimg.cc/GmDMpMT1/Screen-Shot-2023-06-02-at-11-57-11-PM.png" alt="improv2"/>
+                            <br/><br/>
+                        </div>
                         <p><br/>Problem #3: The audit log does not display user-friendly messages for troubleshooting.<br/>
                             Solution: Users can see the progress of each copy under the Job Status column.</p>
-                        <img id="full-width" key="improv3" src="https://i.postimg.cc/B62sc5zW/Screen-Shot-2023-06-02-at-11-54-57-PM.png" alt="improv3"/><br/>
+                        <div className="row">
+                            <img id="full-width" key="improv3" src="https://i.postimg.cc/B62sc5zW/Screen-Shot-2023-06-02-at-11-54-57-PM.png" alt="improv3"/><br/>
+                            <br/><br/>
+                        </div>
                         <p>The biggest improvement to our app was separating the artifacts into their own tabs.
                             This enabled us to easily integrate additional capabilities, such as syncing, reverting, and
                             copying artifacts across IMS organizations.
                         </p><br/>
-                        <img id="full-width" key="improv3" src="https://i.postimg.cc/B62sc5zW/Screen-Shot-2023-06-02-at-11-54-57-PM.png" alt="improv3"/>
+                        <div className="row">
+                            <img id="full-width" key="improv3" src="https://i.postimg.cc/xTNM3Ynn/Screen-Shot-2023-06-05-at-11-06-18-PM.png" alt="improv3"/>
+                            <br/><br/>
+                        </div>
                         <p>Sync capability</p>
-                        <img id="full-width" key="improv3" src="https://i.postimg.cc/B62sc5zW/Screen-Shot-2023-06-02-at-11-54-57-PM.png" alt="improv3"/>
+                        <div className="row">
+                            <img id="full-width" key="improv3" src="https://i.postimg.cc/HkfWmDyW/Screen-Shot-2023-06-05-at-11-07-24-PM.png" alt="improv3"/>
+                            <br/><br/>
+                        </div>
                         <p>Revert capability</p>
-                        <img id="full-width" key="improv3" src="https://i.postimg.cc/B62sc5zW/Screen-Shot-2023-06-02-at-11-54-57-PM.png" alt="improv3"/>
+                        <div className="row">
+                            <img id="full-width" key="improv3" src="https://i.postimg.cc/yYdr1qVR/Screen-Shot-2023-06-05-at-10-51-38-PM.png" alt="improv3"/>
+                            <br/><br/>
+                        </div>
                         <p>Cross IMS organization copy</p><br/>
-                        <p>To satisfy the requirement of logging actions for documentation and troubleshooting, we also
+                        <p>To satisfy the requirement of logging actions for documentation and troubleshooting, we
                             added an Audit Log tab.
                         </p>
-                        <img id="full-width" key="auditLog" src="https://i.postimg.cc/1tMBTX4W/Screen-Shot-2023-06-03-at-12-41-29-AM.png" alt="auditLog"/><br/>
-
-                        <p><h5><br/>Conclusion</h5>
-                            <u>Design Takeaways:</u><br/> In the process of re-designing the app, I learned the importance of
+                        <div className="row">
+                            <img id="full-width" key="auditLog" src="https://i.postimg.cc/1tMBTX4W/Screen-Shot-2023-06-03-at-12-41-29-AM.png" alt="auditLog"/><br/>
+                            <br/><br/>
+                        </div>
+                        </div>
+                        <h5><br/>Conclusion</h5>
+                           <p><u>Design Takeaways:</u><br/> In the process of re-designing the app, I learned the importance of
                             involving the team throughout the design iteration process and communicating clearly with stakeholders
                             to fine-tune requirements. Through modularizing the copy workflows, I also improved
                             my organization skills in design and code. Lastly, by following layouts in AEP, particularly
